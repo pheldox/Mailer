4e53fa84cf748c8735ce776910cb01163fa99f3c
pheldox <felixaryee4@gmail.com>
2020-04-07 00:41:37 +0000

    commented keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,15 @@
-if (process.env.NODE_ENV === 'production') {
-	// in production
-		module.exports = require('./prod')
-} else {
-	// in development
-	module.exports = require('./dev')
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	// in production
+// 		module.exports = require('./prod')
+// } else {
+// 	// in development
+// 	module.exports = require('./dev')
+// }
+module.exports = {
+	googleClientID:
+		'335993886454-d1tusvqo9pmofiacpa4nlu3u562g1sur.apps.googleusercontent.com',
+	googleClientSecret: 'yit-oVqjmh0HfevR36AViSuA',
+	mongoURI:
+		'mongodb+srv://pheldox:apheldox@devcamp-ygoou.mongodb.net/test?retryWrites=true&w=majority',
+	cookieKey: 'qwrtyyukjhjhggfdfgkiyuytdrsdxcvvhjkhjghfgfds',
+};
